fb40b679d559ef28cc144995936039ba809b867e
Derryk Boyd <derryk@redmantech.com>
2024-11-24 00:05:00 -0700

    Add a header to the app route

--- src/routes/app/+layout.svelte
+++ src/routes/app/+layout.svelte
@@ -1,5 +1,17 @@
 <script>
+	import { enhance } from '$app/forms';
+
 	let { children } = $props();
 </script>
 
+<header class="flex justify-between p-4">
+	<h1>Name Picker</h1>
+	<nav>
+		<a href="/">Home</a>
+		<a href="/app">App</a>
+	</nav>
+	<form action="?/logout" method="post" use:enhance>
+		<button>Sign out</button>
+	</form>
+</header>
 {@render children()}
--- src/routes/app/+page.svelte
+++ src/routes/app/+page.svelte
@@ -1,20 +1,19 @@
 <script lang="ts">
 	import { enhance } from '$app/forms';
-	import type { PageServerData } from './$types';
+	import type { ActionData, PageServerData } from './$types';
 
-	let { data }: { data: PageServerData } = $props();
+	let { data, form }: { data: PageServerData; form: ActionData } = $props();
 </script>
 
 <div class="grid gap-3 p-10">
 	<h1>Hi, {data.user.username}!</h1>
-	<p>Your user ID is {data.user.id}.</p>
+	<p>Your user ID is {data.user.id}</p>
 	<p>Your age is {data.user.age}</p>
-	<form method="post" action="?/logout" use:enhance>
-		<button>Sign out</button>
-	</form>
 	<form action="?/setAge" method="post" use:enhance>
-		<input type="number" name="age" />
+		<input required type="number" name="age" />
 		<button>Set age</button>
-		<p>TEST</p>
+		{#if form?.message}
+			<p>{form.message}</p>
+		{/if}
 	</form>
 </div>
