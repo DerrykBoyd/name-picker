d96c91e75d13d693675793a6108e694893288cdf
Derryk Boyd <derryk@redmantech.com>
2024-11-24 01:23:28 -0700

    Change theme, add dark mode

--- src/routes/app/+layout.svelte
+++ src/routes/app/+layout.svelte
@@ -1,5 +1,10 @@
 <script>
 	import { enhance } from '$app/forms';
+	import { Button } from '$lib/components/ui/button';
+	import Sun from 'lucide-svelte/icons/sun';
+	import Moon from 'lucide-svelte/icons/moon';
+
+	import { toggleMode } from 'mode-watcher';
 
 	let { children } = $props();
 </script>
@@ -10,8 +15,18 @@
 		<a href="/">Home</a>
 		<a href="/app">App</a>
 	</nav>
+
+	<Button on:click={toggleMode} variant="outline" size="icon">
+		<Sun
+			class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+		/>
+		<Moon
+			class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+		/>
+		<span class="sr-only">Toggle theme</span>
+	</Button>
 	<form action="?/logout" method="post" use:enhance>
-		<button>Sign out</button>
+		<Button>Sign out</Button>
 	</form>
 </header>
 {@render children()}
