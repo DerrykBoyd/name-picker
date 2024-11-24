ebdddca3c232173191e86a8239eff226c1a711da
Derryk Boyd <derryk@redmantech.com>
2024-11-24 00:35:23 -0700

    Add shadcn svelte

--- src/routes/app/+page.svelte
+++ src/routes/app/+page.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
 	import { enhance } from '$app/forms';
+	import { Button } from '$lib/components/ui/button';
+	import { Input } from '$lib/components/ui/input';
 	import type { ActionData, PageServerData } from './$types';
 
 	let { data, form }: { data: PageServerData; form: ActionData } = $props();
@@ -10,8 +12,8 @@
 	<p>Your user ID is {data.user.id}</p>
 	<p>Your age is {data.user.age}</p>
 	<form action="?/setAge" method="post" use:enhance>
-		<input required type="number" name="age" />
-		<button>Set age</button>
+		<Input required type="number" name="age" />
+		<Button>Set age</Button>
 		{#if form?.message}
 			<p>{form.message}</p>
 		{/if}
