be4cd3a02a9e9424fc66d249ce857a629078a2c1
Derryk Boyd <derryk@redmantech.com>
2024-11-30 01:25:34 -0700

    Fix alignment on home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
 	import { Button } from '$lib/components/ui/button';
 </script>
 
-<div class="grid h-full min-h-screen w-full place-content-center gap-4">
+<div class="grid h-full min-h-screen w-full place-content-center gap-4 p-8 text-center">
 	<h1>Welcome to Name Picker</h1>
 	<p>A completely over engineered app to do something a spreadsheet did just fine.</p>
 	<a href="/app"><Button class="w-fit">Get Started</Button></a>
